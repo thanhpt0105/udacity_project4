0dc4ff5dcf1e24aa4584346f6bfdd715529e7434
thanhpt0105 <thanhpt0105@gmail.com>
2023-11-11 00:55:29 +0800

    Test git action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ export default function App() {
 
   return (
     <div className="container">
-      <h1>Movie List test chagnes</h1>
+      <h1>Movie List</h1>
 
       <MovieList onMovieClick={handleMovieClick} />
 
